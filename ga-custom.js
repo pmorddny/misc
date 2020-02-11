5ab06a8ebc0c5682a2c6e59530ab19d5945e0754
pmorddny <pmorddny@gmail.com>
2020-02-11 11:26:11 +0000

    Create ga-custom.js

---
+++ ga-custom.js
@@ -0,0 +1,15 @@
+(function(window, document, videojs) {
+
+    // map and indexOf polyfills for obsolete browsers
+    Array.prototype.map||(Array.prototype.map=function(r,t){var n,e,o;if(null==this)throw new TypeError(" this is null or not defined");var i=Object(this),a=i.length>>>0;if("function"!=typeof r)throw new TypeError(r+" is not a function");for(arguments.length>1&&(n=t),e=new Array(a),o=0;a>o;){var f,p;o in i&&(f=i[o],p=r.call(n,f,o,i),e[o]=p),o++}return e});
+    Array.prototype.indexOf||(Array.prototype.indexOf=function(r,t){var n;if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),i=e.length>>>0;if(0===i)return-1;var a=+t||0;if(1/0===Math.abs(a)&&(a=0),a>=i)return-1;for(n=Math.max(a>=0?a:i-Math.abs(a),0);i>n;){if(n in e&&e[n]===r)return n;n++}return-1});
+  
+    var gacustom = function(settings) {
+      var player = this;
+      
+        debugger;
+        window.ga('gaTracker.set', 'dimension1', 'mediaocean.com' );        
+    }
+  
+    videojs.registerPlugin('ga-custom', gacustom );
+  })(window, document, videojs);
