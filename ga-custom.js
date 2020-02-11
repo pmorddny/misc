7c2cb465e5ef71bcb90e5420b6ebdac03357c2c9
pmorddny <pmorddny@gmail.com>
2020-02-11 15:55:50 +0000

    Update ga-custom.js

--- ga-custom.js
+++ ga-custom.js
@@ -5,10 +5,24 @@
     Array.prototype.indexOf||(Array.prototype.indexOf=function(r,t){var n;if(null==this)throw new TypeError('"this" is null or not defined');var e=Object(this),i=e.length>>>0;if(0===i)return-1;var a=+t||0;if(1/0===Math.abs(a)&&(a=0),a>=i)return-1;for(n=Math.max(a>=0?a:i-Math.abs(a),0);i>n;){if(n in e&&e[n]===r)return n;n++}return-1});
   
     var gacustom = function(settings) {
-      var player = this;
-      
+        var getDomain;
+        
         debugger;
-        window.ga('gaTracker.set', 'dimension1', 'mediaocean.com' );        
+        if (
+        window.ga('gaTracker.set', 'dimension1', HelpCenter.user.email );        
+        
+        getDomain = function(email) {
+           if ( email === null ) { return ''; }
+
+            const split = email.split('@');
+
+            if (split.length > 1 ) {
+                const domain = split[1];
+                return domain;
+                }
+
+            return '';
+        }
     }
   
     videojs.registerPlugin('ga-custom', gacustom );
